608fe2271d6addd949ff4c395309bda84fc6cac6
Marcus Herrera <maherreraiv1987@gmail.com>
2025-05-10 16:12:57 -0600

    Book model init

--- csharp-books.Server/Models/Book.cs
+++ csharp-books.Server/Models/Book.cs
@@ -1,6 +1,18 @@
-﻿namespace csharp_books.Server.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace csharp_books.Server.Models
 {
     public class Book
     {
+        [Display(Name = "id")]
+        public int ID { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string description { get; set; }
+        public int copies { get; set; }
+        public int copiesAvailable { get; set; }
+        public string category { get; set; }
+        public string img { get; set; }
     }
 }
