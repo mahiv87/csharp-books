96915a59508d8e23a8051be7720a88e3a5d14079
Marcus mbp <maherreraiv1987@gmail.com>
2025-06-04 19:56:05 -0600

    book controller init

---
+++ csharp-books.Server/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+namespace csharp_books.Server.Controllers
+{
+  public class BookController : BaseController
+  {
+    // This controller will handle book-related requests.
+    // You can implement methods to get, add, update, and delete books here.
+    // For example:
+    
+    // [HttpGet]
+    // public IActionResult GetBooks()
+    // {
+    //     // Logic to retrieve books from the database
+    // }
+    
+    // [HttpPost]
+    // public IActionResult AddBook(Book book)
+    // {
+    //     // Logic to add a new book to the database
+    // }
+  }
+}
